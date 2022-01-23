ee5fe22d1f8e3a2c2c9f4c721c1d81b90968123e
Okan <okso2004@gmail.com>
2022-01-24 00:14:47 +0100

    Initial Commit

---
+++ Shared/Password_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_GeneratorApp.swift
+//  Shared
+//
+//  Created by Okan Sönmez on 24.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Password_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
